e340dd0e6de87a435c0d2d89f453a82fa52363a2
Piotr Dolecki <pjotair.test@gmail.com>
2023-08-28 14:33:23 +0200

    change description

--- python_exercises/text_caseuses/palindrome/check_palindrome_use_lambda.py
+++ python_exercises/text_caseuses/palindrome/check_palindrome_use_lambda.py
@@ -5,7 +5,7 @@ Get a word from the user and check if it is a palindrome.
 check_palindrome = lambda text: print(f'{text} is a palindrome') if text == text[::-1] else print(f'{text} is not a palindrome')
 
 if __name__ == "__main__":
-    print('opisać co tu sie dzieje, naciś kontrol c aby przrwać')
-    text = input('Enter the word: ')
+    print('This is a program that checks if the given word is a palindrome.\nTo end the action early press ctrl + c')
+    text = input('Enter the word here: ')
 
     check_palindrome(text)
