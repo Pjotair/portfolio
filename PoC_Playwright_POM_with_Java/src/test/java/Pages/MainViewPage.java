0d15828626411eb6dc03a373e005971263c12f00
Piotr Dolecki <81429526+Pjotair@users.noreply.github.com>
2024-01-03 19:49:16 +0100

    add tests and readme (#7)

--- PoC_Playwright_POM_with_Java/src/test/java/Pages/MainViewPage.java
+++ PoC_Playwright_POM_with_Java/src/test/java/Pages/MainViewPage.java
@@ -1,12 +1,19 @@
 package Pages;
 
+import com.microsoft.playwright.*;
+import static com.microsoft.playwright.Locator.*;
+
+
 public class MainViewPage {
     public static String baseUrl = "https://fakestore.testelka.pl/";
 
     // Locators
-    public static String HOME_PAGE_TITLE = "div[class='content-area'] header h1";
+    public static String LOGO = "div a[class='custom-logo-link']";
     public static String MENU_CONTAINER = "div[class='primary-navigation']";
     public static String MENU_ELEMENTS = "ul[id='menu-menu'] li";
+    public static String HOME_PAGE_TITLE = "div[class='content-area'] header h1";
+    public static String FIND_ELEMENTS = "div[class='content-area'] li h2";
+    public static String SEARCH_INPUT = "input[id='woocommerce-product-search-field-0']";
 
     // Expected data
     public static String mainPageHeader = "Wybierz podróż dla siebie!";
