accf24b21fd84a94a1a3660a8ed744cdc7c1c143
Piotr Dolecki <pjotair.test@gmail.com>
2024-01-03 19:47:31 +0100

    add tests and readme

--- PoC_Playwright_POM_with_Java/src/test/java/Tests/TestMainView.java
+++ PoC_Playwright_POM_with_Java/src/test/java/Tests/TestMainView.java
@@ -1,6 +1,7 @@
 package Tests;
 
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.LoadState;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
 
@@ -39,16 +40,19 @@ public class TestMainView {
     }
 
     @Test
-    void checkPageHeader() {
+    void checkLogo() {
         /*
-        The user sees the page header
+        The user sees the page logo
         Given: the user has access to the page
         When: open the home page
-        Then: the page contains the header
+        Then: the page contains the logo
         */
-        Locator pageHeader = page.locator(HOME_PAGE_TITLE);
-        assertThat(pageHeader).containsText(mainPageHeader);
-        }
+        Locator logo = page.locator(LOGO);
+        assertThat(logo).isVisible();
+        logo.click();
+        assertThat(page).hasURL(baseUrl);
+    }
+
     @Test
     void checkNavigateList() {
         /*
@@ -66,5 +70,42 @@ public class TestMainView {
             actualList[i] = listItems[i].innerText();
         }
         assertArrayEquals(expectedMenuList, actualList);
+    }
+
+    @Test
+    void checkPageHeader() {
+        /*
+        The user sees the page header
+        Given: the user has access to the page
+        When: open the home page
+        Then: the page contains the header
+        */
+        Locator pageHeader = page.locator(HOME_PAGE_TITLE);
+        assertThat(pageHeader).containsText(mainPageHeader);
+    }
+
+    @Test
+    void checkSearch() {
+        /*
+        The user can search for products
+        Given: the user has access to the page
+        When: Provides the search phrase
+        Then: the page displays the search results
+        */
+        Locator searchInput = page.locator(SEARCH_INPUT);
+        String searchedPhrase = "windsurfing";
+        searchInput.type(searchedPhrase);
+        page.press(SEARCH_INPUT, "Enter");
+        page.waitForLoadState(LoadState.NETWORKIDLE);
+
+        ElementHandle[] elements = page.querySelectorAll(FIND_ELEMENTS).toArray(new ElementHandle[0]);
+        page.waitForTimeout(500);
+        if (elements.length > 0) {
+            for (ElementHandle element : elements) {
+                String currentText = element.innerText();
+                String lowerText = currentText.toLowerCase();
+                assert lowerText.contains(searchedPhrase);
+            }
+        }
     }
 }
